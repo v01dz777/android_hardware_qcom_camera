4637f7c6626d863833f3b56e7d33be9090d14e82
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-12 16:48:47 -0400

    camera: Only build if specified
    This is a combination of 2 commits.
    The first commit's message is:
    
    camera: put back USE_DEVICE_SPECIFIC_CAMERA
    
    Authored-by: Iliyan Malchev <malchev@google.com>
    Change-Id: I7b992e5bad7bb807fcdb379553b52f653a1c245b
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    This is the 2nd commit message:
    
    camera: Use project pathmap
    
    Authored-by: Tom Marshall <tdm@cyngn.com>
    Change-Id: I7c79b4117ebcea147c57afa19d81d632d07478db

--- Android.mk
+++ Android.mk
@@ -1,14 +1,13 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 
-ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
-  ifneq ($(USE_CAMERA_STUB),true)
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
+endif
+
 endif
